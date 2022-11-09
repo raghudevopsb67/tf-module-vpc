ba5c2b1337cdea5c6de00764a26aef13ea0c8a76
Raghu K <raghuk.vit@gmail.com>
2022-11-09 07:16:27 +0530

    Update tf-module-vpc/

--- subnets.tf
+++ subnets.tf
@@ -31,5 +31,5 @@ module "private_subnets" {
 //}
 
 output "all_private_subnets" {
-  value = [for k, v in module.private_subnets : v.subnets.app.subnets]
+  value = [for k, v in module.private_subnets : v.subnets]
 }
