3f3793d9bafd86b659e77f3275cea4e975900bc9
Raghu K <raghuk.vit@gmail.com>
2022-11-10 06:41:02 +0530

    Update tf-module-vpc/

--- gw.tf
+++ gw.tf
@@ -22,19 +22,16 @@ resource "aws_eip" "ngw" {
 
 locals {
   private_route_tables = [for i, j in module.private_subnets : j.rt]
+  public_route_tables  = [for i, j in module.public_subnets : j.rt]
 }
 
-output "test" {
-  value = local.private_route_tables
-}
 
-//
-//resource "aws_route" "internet_gateway_route_to_public_subnets" {
-//  count = length()
-//  route_table_id            = element(???, count.index)
-//  destination_cidr_block    = each.value.cidr_block
-//  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
-//}
+resource "aws_route" "internet_gateway_route_to_public_subnets" {
+  count                  = length(local.public_route_tables)
+  route_table_id         = element(local.public_route_tables, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
 
 
 
