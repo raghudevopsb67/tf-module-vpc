626530d6a3af9101e028115c2ee074e0ef3b7e38
Raghu K <raghuk.vit@gmail.com>
2022-11-10 06:46:46 +0530

    Update tf-module-vpc/

--- subnets/main.tf
+++ subnets/main.tf
@@ -33,13 +33,13 @@ resource "aws_route" "peering_connection_route" {
 //  vpc_peering_connection_id = var.peering_connection_id
 //}
 
-//locals {
-//  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
-//}
-//
-//output "subnets" {
-//  value = local.subnets_list[*].id
-//}
+locals {
+  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
+}
+
+output "subnets_list" {
+  value = local.subnets_list[*].id
+}
 
 //output "subnets" {
 //  value = module.lm-subnets
