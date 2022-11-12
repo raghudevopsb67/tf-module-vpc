d214ac8813baddaf41cf25bf0abe534423ffbe63
Raghu K <raghuk.vit@gmail.com>
2022-11-12 06:44:53 +0530

    Update tf-module-vpc/

--- subnets/main.tf
+++ subnets/main.tf
@@ -44,11 +44,11 @@ resource "aws_route_table" "aws_route_table" {
   }
 }
 
-//resource "aws_route_table_association" "route-table-association" {
-//  count          = length(aws_subnet.main)
-//  subnet_id      = element(aws_subnet.main.*.id, count.index)
-//  route_table_id = var.route_table_id
-//}
+resource "aws_route_table_association" "route-table-association" {
+  count          = length(aws_subnet.main)
+  subnet_id      = element(aws_subnet.main.*.id, count.index)
+  route_table_id = aws_route_table.aws_route_table.id
+}
 
 
 
