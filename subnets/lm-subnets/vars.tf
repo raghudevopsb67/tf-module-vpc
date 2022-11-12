4f665e62698034aa160f9a73060d7d5b13dc6911
Raghu K <raghuk.vit@gmail.com>
2022-11-12 07:19:19 +0530

    Update tf-module-vpc/

--- subnets/lm-subnets/vars.tf
+++
@@ -1,6 +0,0 @@
-variable "cidr_block" {}
-variable "vpc_id" {}
-variable "env" {}
-variable "name" {}
-variable "subnet_availability_zones" {}
-variable "route_table_id" {}
--- subnets/main.tf
+++ subnets/main.tf
@@ -1,32 +1,3 @@
-//module "lm-subnets" {
-//  for_each                  = var.subnets
-//  cidr_block                = each.value.cidr_block
-//  source                    = "./lm-subnets"
-//  vpc_id                    = var.vpc_id[0]
-//  env                       = var.env
-//  name                      = each.value.name
-//  subnet_availability_zones = var.subnet_availability_zones
-//  route_table_id            = lookup(lookup(aws_route_table.aws_route_table, each.value.name, null), "id", null)
-//}
-
-//resource "aws_route_table" "aws_route_table" {
-//  for_each = var.subnets
-//  vpc_id   = var.vpc_id[0]
-//  tags = {
-//    Name    = "${var.env}-${each.value.name}-rt"
-//    ENV     = var.env
-//    PROJECT = "roboshop"
-//  }
-//}
-//
-//resource "aws_route" "peering_connection_route" {
-//  for_each                  = var.subnets
-//  route_table_id            = lookup(lookup(aws_route_table.aws_route_table, each.value.name, null), "id", null)
-//  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
-//  vpc_peering_connection_id = var.peering_connection_id
-//}
-//
-
 resource "aws_subnet" "main" {
   count             = length(var.cidr_block)
   vpc_id            = var.vpc_id
@@ -54,6 +25,11 @@ resource "aws_route" "internet_gateway_route" {
   route_table_id         = aws_route_table.aws_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = var.gateway_id
+  lifecycle {
+    ignore_changes = [
+      gateway_id
+    ]
+  }
 }
 
 
