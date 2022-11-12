e16e43c9bb241197b133108a85f7cf8d7cd7f6cc
Raghu K <raghuk.vit@gmail.com>
2022-11-12 07:04:38 +0530

    Update tf-module-vpc/

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,10 @@ output "private_subnets" {
   value = module.private_subnets
 }
 
-//output "public_subnets" {
-//  value = module.public_subnets
-//}
-//
-//
+output "public_subnets" {
+  value = module.public_subnets
+}
+
 
 output "test" {
   value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
