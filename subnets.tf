12b157f69c6beb6c1360156deb33256b22e1b790
Raghu K <raghuk.vit@gmail.com>
2022-11-12 06:40:47 +0530

    Update tf-module-vpc/

--- subnets.tf
+++ subnets.tf
@@ -6,9 +6,9 @@ module "public_subnets" {
   env                       = var.env
   name                      = each.value.name
   subnet_availability_zones = var.subnet_availability_zones
-  peering_connection_id     = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
-  management_vpc            = var.management_vpc
-  internet_gateway_id       = aws_internet_gateway.gw.*.id[0]
+  //peering_connection_id     = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
+  //management_vpc            = var.management_vpc
+  //internet_gateway_id       = aws_internet_gateway.gw.*.id[0]
 }
 
 //module "private_subnets" {
